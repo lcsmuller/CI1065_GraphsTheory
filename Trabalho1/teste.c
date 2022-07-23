3297f8e3f0c017eca43aca176a9eede139aafa37
guipateiro <guilhermepateiro@gmail.com>
2022-07-23 12:50:30 -0300

    feito o n_triangulos

--- Trabalho1/teste.c
+++ Trabalho1/teste.c
@@ -9,11 +9,26 @@ main(void)
     printf(" numero de vertices do grafo: %i \n", n_vertices(g));
     printf(" numero de arestas do grafo: %i \n", n_arestas(g));
     printf(" grau maximo: %i | grau mínimo: %i | grau médio: %i \n",
-	   grau_maximo(g), grau_minimo(g), grau_medio(g));
+	   	grau_maximo(g), grau_minimo(g), grau_medio(g));
+    printf(" numero de triangulos do grafo: %i \n", n_triangulos(g));
     printf(" regular: %i | completo: %i \n", regular(g), completo(g));
 
     escreve_grafo(g);
-    destroi_grafo(g);
+    //destroi_grafo(g);
 
+    grafo comp = complemento(g); 
+	printf(" numero de vertices do grafo: %i \n", n_vertices(comp));
+    printf(" numero de arestas do grafo: %i \n", n_arestas(comp));
+    printf(" grau maximo: %i | grau mínimo: %i | grau médio: %i \n",
+		grau_maximo(comp), grau_minimo(comp), grau_medio(comp));
+    printf(" numero de triangulos do grafo: %i \n", n_triangulos(g));
+    printf(" regular: %i | completo: %i \n", regular(comp), completo(comp));
+
+    int **matriz = matriz_adjacencia(g); 
+    imprime_matriz_adjacencia(matriz,n_vertices(g));
+	escreve_grafo(comp);
+    destroi_grafo(comp);
+	destroi_grafo(g);
+    
     return 0;
 }
